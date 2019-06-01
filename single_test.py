00b8147aa6995ca10cddaf491a5da411dcff99dc
yjt <961549745@qq.com>
2019-06-01 11:37:22 +0800

    单次调用也输出到log

--- single_test.py
+++ single_test.py
@@ -1,3 +1,10 @@
 from get_connect_time import get_connect_time
+import time
+from time import strftime, localtime
+import logging
 
-print(get_connect_time())
+logging.basicConfig(filename='connect.log', level=logging.INFO)
+logging_str = strftime("[%Y-%m-%d %H:%M:%S]: ",
+                       localtime(time.time())) + get_connect_time()
+logging.info(logging_str)
+print(logging_str)
